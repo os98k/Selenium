b3f08afabed54715737b41882a6cbe35968d7d78
Osama Shakeel <83074369+os98k@users.noreply.github.com>
2022-06-04 15:49:41 +0500

    Update css-selector.py

--- css-selector.py
+++ css-selector.py
@@ -3,7 +3,6 @@ import time
 
 driver = webdriver.Chrome(executable_path= "C:\\Users\\Global Computers\\Desktop\Selenium\\chromedriver.exe")
 
-
 driver.get("https://www.rahulshettyacademy.com/AutomationPractice/")
 #tagname[attribute = 'value']
 driver.find_element_by_css_selector("input[value= 'radio1']").click()
