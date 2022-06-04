42f320a251e656a4803390c0a6e4c65da1f06c1b
Osama Shakeel <83074369+os98k@users.noreply.github.com>
2022-06-04 15:48:26 +0500

    Add files via upload

---
+++ css-selector.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome(executable_path= "C:\\Users\\Global Computers\\Desktop\Selenium\\chromedriver.exe")
+
+
+driver.get("https://www.rahulshettyacademy.com/AutomationPractice/")
+#tagname[attribute = 'value']
+driver.find_element_by_css_selector("input[value= 'radio1']").click()
+driver.find_element_by_css_selector("#name").send_keys("osama")
